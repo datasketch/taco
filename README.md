22e11be4865acd8a22dce14c5a06f040aac5ebe5
David Daza <03dazal@gmail.com>
2023-05-16 17:03:19 -0500

    validate dictionary is an array

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## taco
 
 Tabulator (auto) configuration
+
+### Usage
+
+```html
+<script src="https://unpkg.com/@datasketch/taco@0.1.0/dist/taco.js"></script>
+```
--- src/columns.js
+++ src/columns.js
@@ -1,4 +1,7 @@
 function getColumnDefinitions (dictionary) {
+  if (!Array.isArray(dictionary)) {
+    throw new Error('Dictionary must be an array')
+  }
   return dictionary.map(field => ({
     title: field.label,
     field: field.id
